fbf1b5b2ec89fd840db7170d324ffc791fd088b7
Robert <taeil.choi@kt.com>
2013-11-15 14:34:47 +0900

    refactor per-env attrs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,14 @@
 include_attribute "graphite"
 include_attribute "ktc-sensu"
-include_attribute "ktc-monitor::default"
 include_attribute "ktc-gdash::default"
 
 default[:monitor][:recipes_server_collectd] = %w{
-  graphite
   collectd::server
   ktc-collectd::client_graphite
 }
 
 default[:monitor][:recipes_server_graphite] = %w{
+  graphite
 }
 
 default[:monitor][:recipes_server_sensu] = %w{
@@ -17,6 +16,7 @@ default[:monitor][:recipes_server_sensu] = %w{
 }
 
 default[:monitor][:recipes_server_gdash] = %w{
+  ktc-gdash
 }
 
 default[:monitor][:recipes_client] = %w{
@@ -24,20 +24,18 @@ default[:monitor][:recipes_client] = %w{
   ktc-sensu::client
 }
 
-# TODO: This endpoint should be handled with Services library in ktc-sensu.
-default[:sensu][:graphite_address] = "localhost"
-default[:sensu][:graphite_port] = 2003
-
-# TODO: This endpoint should be handled with Services library in ktc-gdash.
-# This url should be IP address, not hostname.
-default[:gdash][:graphite_url] = "http://localhost"
-default[:gdash][:title] = "Test"
-
-
 default[:monitor][:splunk][:ip] = ""
 default[:monitor][:splunk][:port] = ""
 
-default[:sensu][:hipchat_room] = "test-alert"
+# TODO: This endpoint should be handled with Services library in ktc-sensu.
+default[:sensu][:graphite_address] = "localhost"
+default[:sensu][:graphite_port] = 2003
+default[:sensu][:hipchat_room] = "alert-test"
+
+# TODO: This endpoint should be handled with Services library in ktc-gdash.
+# This url should be IP address, not hostname.
+default[:gdash][:graphite_url] = "http://127.0.0.1"
+default[:gdash][:title] = "Test"
 
 default[:graphite][:storage_schemas] = [
   {
--- attributes/ipc-ng.rb
+++ attributes/ipc-ng.rb
@@ -1,32 +1,15 @@
 #
-# Attributes for ipc_ng environment
+# Attributes for ipc-ng environment
 #
 
-return unless chef_environment == "ipc_ng"
+return unless chef_environment == "ipc-ng"
 
 include_attribute "ktc-monitor::default"
 
 default[:monitor][:recipes_server_collectd] = %w{
-  graphite
   collectd::server
   ktc-collectd::client_graphite
   ktc-collectd::client_splunk
-}
-
-default[:monitor][:recipes_server_graphite] = %w{
-}
-
-default[:monitor][:recipes_server_sensu] = %w{
-  ktc-sensu::master
-  ktc-gdash
-}
-
-default[:monitor][:recipes_server_gdash] = %w{
-}
-
-default[:monitor][:recipes_client] = %w{
-  ktc-collectd::client_collectd
-  ktc-sensu::client
 }
 
 # Forward metrics to Splunk
@@ -34,9 +17,11 @@ default[:monitor][:splunk][:ip] = "10.2.2.81"
 default[:monitor][:splunk][:port] = "4110"
 
 # TODO: This endpoint shoudl be handled with Services library in ktc-sensu.
-default[:sensu][:graphite_address] = "monitor01-vm.mgmt1.ipc-ng"
+default[:sensu][:graphite_address] = "graphite01-vm.mgmt1.ipc-ng"
+default[:sensu][:hipchat_room] = "alert-ipc-ng"
 
 # TODO: This endpoint should be handled with Services library in ktc-gdash.
+# Until then, manually set graphite IP here or environment file.
 # This url should be IP address, not hostname.
 default[:gdash][:graphite_url] = ""
 default[:gdash][:title] = "IPC-NG"
