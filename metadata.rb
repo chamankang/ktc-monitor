e8ed3abc2be6d3cb2ac35cd8973ea08f9983371e
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2013-12-24 08:43:24 +0900

    auto_version to 1.0.7

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "ktc-monitor"
 maintainer        "Robert Choi"
 license           "Apache 2.0"
 description       "monitoring cookbook"
-version '1.0.6'
+version '1.0.7'
 
 %w{ centos ubuntu }.each do |os|
     supports os
